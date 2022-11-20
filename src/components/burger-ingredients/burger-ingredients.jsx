f725b3bd3f146bbf8c7760e0b3b739ea54d35d48
temarazin <temarazin@gmail.com>
2022-11-20 13:33:51 +0300

    feat: add ingredient scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './burger-ingredients.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurderIngredientCategory from '../burger-ingredient-category/burger-ingredient-category';
@@ -10,7 +10,50 @@ import { getIngredients, SET_CURRENT_INGREDIENT } from '../../services/actions/i
 function BurgerIngredients() {
   const dispatch = useDispatch();
 
+  const catBun = useRef();
+  const catSauce = useRef();
+  const catMain = useRef();
+
   const { ingredients, currentIngredient } = useSelector(store => store.ingredients);
+
+  function onTabClickHandler(tab) {
+    setCurrent(tab);
+    switch (tab) {
+      case 'bun':
+        catBun.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'sauce':
+        catSauce.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      case 'main':
+        catMain.current.scrollIntoView({ behavior: 'smooth' });
+        break;
+      default:
+        break;
+    }
+  }
+
+  // TODO: переделать этот ужас
+  const onMenuScroll = (e) => {
+    const containerTop = e.target.getBoundingClientRect().y;
+    const catBunTop = Math.abs(catBun.current.getBoundingClientRect().y - containerTop);
+    const catSauceTop = Math.abs(catSauce.current.getBoundingClientRect().y - containerTop);
+    const catMainTop = Math.abs(catMain.current.getBoundingClientRect().y - containerTop);
+    const min = Math.min(catBunTop, catSauceTop, catMainTop);
+    switch(min) {
+      case catBunTop:
+        setCurrent('bun');
+        break;
+      case catSauceTop:
+        setCurrent('sauce');
+        break;
+      case catMainTop:
+        setCurrent('main');
+        break;
+      default:
+        setCurrent('bun')
+    }
+  }
 
   const [current, setCurrent] = React.useState('one')
   const [isModalOpened, setIsModalOpened] = useState(false);
@@ -42,22 +85,22 @@ function BurgerIngredients() {
           Соберите бургер
         </h1>
         <div className={`${styles.tabs} mb-10`}>
-          <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+          <Tab value="bun" active={current === 'bun'} onClick={onTabClickHandler}>
             Булки
           </Tab>
-          <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+          <Tab value="sauce" active={current === 'sauce'} onClick={onTabClickHandler}>
             Соус
           </Tab>
-          <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+          <Tab value="main" active={current === 'main'} onClick={onTabClickHandler}>
             Начинки
           </Tab>
         </div>
-        <div className={styles.content}>
-          <h2 className="text text_type_main-medium">Булки</h2>
+        <div className={styles.content} onScroll={onMenuScroll}>
+          <h2 className="text text_type_main-medium" ref={catBun}>Булки</h2>
           <BurderIngredientCategory data={ingredients.filter(item => item.type === 'bun')} onIndegrientClick={showIngredient} />
-          <h2 className="text text_type_main-medium">Соусы</h2>
+          <h2 className="text text_type_main-medium" ref={catSauce}>Соусы</h2>
           <BurderIngredientCategory data={ingredients.filter(item => item.type === 'sauce')} onIndegrientClick={showIngredient} />
-          <h2 className="text text_type_main-medium">Начинки</h2>
+          <h2 className="text text_type_main-medium" ref={catMain}>Начинки</h2>
           <BurderIngredientCategory data={ingredients.filter(item => item.type === 'main')} onIndegrientClick={showIngredient} />
         </div>
       </section>
